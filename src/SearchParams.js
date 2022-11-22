4d7e4abc9e7a5afce4cc38a106cfb104d407d1fc
TomBirbeck <tombirbeck47@gmail.com>
2022-11-22 18:26:30 +0000

    changed the event listeners to use the dispatch for redux with the action creators

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
 import Results from './Results';
 import useBreedList from './useBreedList';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import changeAnimal from './actionCreators/changeAnimal';
+import changeBreed from './actionCreators/changeBreed';
+import changeLocation from './actionCreators/changeLocation';
+import changeTheme from './actionCreators/changeTheme';
 
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 
@@ -14,6 +18,7 @@ const SearchParams = () => {
   //not redux
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     requestPets();
@@ -42,7 +47,7 @@ const SearchParams = () => {
             id='location'
             value={location}
             placeholder='Location'
-            onChange={(e) => updateLocation(e.target.value)}
+            onChange={(e) => dispatch(changeLocation(e.target.value))}
           />
         </label>
         <label htmlFor='animal'>
@@ -51,12 +56,10 @@ const SearchParams = () => {
             id='animal'
             value={animal}
             onChange={(e) => {
-              updateAnimal(e.target.value);
-              updateBreed('');
+              dispatch(changeAnimal(e.target.value));
             }}
             onBlur={(e) => {
-              updateAnimal(e.target.value);
-              updateBreed('');
+              dispatch(changeAnimal(e.target.value));
             }}
           >
             <option />
@@ -73,8 +76,8 @@ const SearchParams = () => {
             disabled={!breeds.length}
             id='breed'
             value={breed}
-            onChange={(e) => updateBreed(e.target.value)}
-            onBlur={(e) => updateBreed(e.target.value)}
+            onChange={(e) => dispatch(changeBreed(e.target.value))}
+            onBlur={(e) => dispatch(changeBreed(e.target.value))}
           >
             <option />
             {breeds.map((breed) => (
@@ -88,8 +91,8 @@ const SearchParams = () => {
           Theme
           <select
             value={theme}
-            onChange={(e) => setTheme(e.target.value)}
-            onBlur={(e) => setTheme(e.target.value)}
+            onChange={(e) => dispatch(changeTheme(e.target.value))}
+            onBlur={(e) => dispatch(changeTheme(e.target.value))}
           >
             <option value='peru'>Peru</option>
             <option value='darkblue'>Dark Blue</option>
--- src/store.js
+++ src/store.js
@@ -1,11 +1,10 @@
 import { createStore } from 'redux';
-import reducer from './reducers.js';
+import reducer from './reducers';
 
 const store = createStore(
   reducer,
-  //this enables redux dev tools
   typeof window === 'object' &&
-    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === 'undefined'
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
     ? window.__REDUX_DEVTOOLS_EXTENSION__()
     : (f) => f
 );
