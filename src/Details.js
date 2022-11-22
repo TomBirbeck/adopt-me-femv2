86c9f343983e1ae355ea22e6228ff9f85af7b2e5
TomBirbeck <tombirbeck47@gmail.com>
2022-11-22 18:33:28 +0000

    brought redux into details, used older style to create the reduxwrappeddetails and pass it in so it's displayed

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,9 @@
-import { Component } from "react";
-import { useParams } from "react-router-dom";
-import Carousel from "./Carousel";
-import ErrorBoundary from "./ErrorBoundary";
-import ThemeContext from "./ThemeContext";
-import Modal from "./Modal";
+import { Component } from 'react';
+import { useParams } from 'react-router-dom';
+import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
+import Modal from './Modal';
+import { connect } from 'react-redux';
 
 class Details extends Component {
   state = { loading: true, showModal: false };
@@ -16,7 +16,7 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
   toggleModal = () => this.setState({ showModal: !this.state.showModal });
-  adopt = () => (window.location = "http://bit.ly/pet-adopt");
+  adopt = () => (window.location = 'http://bit.ly/pet-adopt');
   render() {
     if (this.state.loading) {
       return <h2>loading … </h2>;
@@ -26,28 +26,26 @@ class Details extends Component {
       this.state;
 
     return (
-      <div className="details">
+      <div className='details'>
         <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-          <ThemeContext.Consumer>
-            {([theme]) => (
-              <button
-                onClick={this.toggleModal}
-                style={{ backgroundColor: theme }}
-              >
-                Adopt {name}
-              </button>
-            )}
-          </ThemeContext.Consumer>
+
+          <button
+            onClick={this.toggleModal}
+            style={{ backgroundColor: this.props.theme }}
+          >
+            Adopt {name}
+          </button>
+
           <p>{description}</p>
           {showModal ? (
             <Modal>
               <div>
                 <h1>Would you like to adopt {name}?</h1>
-                <div className="buttons">
-                  <a href="https://bit.ly/pet-adopt">Yes</a>
+                <div className='buttons'>
+                  <a href='https://bit.ly/pet-adopt'>Yes</a>
                   <button onClick={this.toggleModal}>No</button>
                 </div>
               </div>
@@ -59,11 +57,21 @@ class Details extends Component {
   }
 }
 
+const mapStateToProps = ({ theme }) => ({ theme });
+
+/*
+function(props){
+  return {theme: props.theme}
+}
+*/
+
+const ReduxWrappedDetails = connect(mapStateToProps)(Details);
+
 const WrappedDetails = () => {
   const params = useParams();
   return (
     <ErrorBoundary>
-      <Details params={params} />
+      <ReduxWrappedDetails params={params} />
     </ErrorBoundary>
   );
 };
