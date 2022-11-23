320247a6b3cda9d94aa63df3155116308372d8d0
TomBirbeck <tombirbeck47@gmail.com>
2022-11-23 07:49:29 +0000

    added thumbnail tests for pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,19 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Pet = (props) => {
   const { name, animal, breed, images, location, id } = props;
 
-  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
-  if (images.length) {
+  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg';
+  if (Array.isArray(images) && images.length) {
     hero = images[0];
   }
 
   return (
-    <Link to={`/details/${id}`} className="pet">
-      <div className="image-container">
-        <img src={hero} alt={name} />
+    <Link to={`/details/${id}`} className='pet'>
+      <div className='image-container'>
+        <img src={hero} alt={name} data-testid='thumbnail' />
       </div>
-      <div className="info">
+      <div className='info'>
         <h1>{name}</h1>
         <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
